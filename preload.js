24ee9c627d67e193c5f4d1c1aa10e1ccbc2378de
ryuhc0726-design <ryuhc0726@gmail.com>
2026-06-13 18:04:38 +0900

    Delete preload.js

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  getCsvPath: () => ipcRenderer.invoke('get-csv-path'),
-  readCsv: () => ipcRenderer.invoke('read-csv'),
-  writeCsv: (content) => ipcRenderer.invoke('write-csv', content),
-  logError: (msg) => ipcRenderer.send('log-error', msg)
-});
